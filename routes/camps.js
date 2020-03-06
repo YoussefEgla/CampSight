a9f0555073a46a5d04d0f11f684dcc718b9eab9e
Youssef AbouEgla <youssefegla@gmail.com>
2020-03-06 15:24:15 +0200

    added camps route

--- routes/camps.js
+++ routes/camps.js
@@ -6,11 +6,19 @@ const
     router = express.Router();
 
 /**
+ *  temporary data
+ */
+const camps = [
+    { name: "Camp 1", url: "https://images.unsplash.com/photo-1530541930197-ff16ac917b0e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" },
+    { name: "Camp 2", url: "https://images.unsplash.com/photo-1504851149312-7a075b496cc7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=649&q=80" },
+    { name: "Camp 3", url: "https://images.unsplash.com/photo-1528892677828-8862216f3665?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" }
+]
+/**
 * Routes
 */
 router.route('/')
     .get((req, res) => {
-        res.send("List All Camps");
+        res.render('camps', { camps })
     });
 
 module.exports = router
